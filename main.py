d928a82aa426ee5df051988cdaa9dd9cfe668be6
David Deist <david.deist@snhu.edu>
2026-07-29 14:35:19 -0400

    Added the 'load_cards()' function, as well as some basic, testing print-outs.

--- main.py
+++ main.py
@@ -1,14 +1,44 @@
 from src.api.mtg_api import fetch_cards
-from src.storage.json_storage import save_cards
+from src.storage.json_storage import save_cards, load_cards
 
 def main():
     cards = fetch_cards()
 
     save_cards(cards)
 
-    print(cards[0].keys())
-    print(cards[0]["name"])
-    print(len(cards))
+    loaded_cards = load_cards(filename="data/cards.json")
+
+    print(f"Loaded {len(loaded_cards)} cards from data/cards.json")
+
+    print("First Card:")
+    print(f"Name: {loaded_cards[0]['name']}")
+    print(f"Colors: {loaded_cards[0]['colors']}")
+    print(f"Converted Mana Cost: {loaded_cards[0]['cmc']}")
+    print(f"Power: {loaded_cards[0]['power']}")
+    print(f"Toughness: {loaded_cards[0]['toughness']}")
+    if 'supertypes'in loaded_cards[0]:
+        print(f"Supertypes: {loaded_cards[0]['supertypes']}")
+    else:
+        print("Supertypes: None")
+    print(f"Subtypes: {loaded_cards[0]['subtypes']}")
+    print(f"Types: {loaded_cards[0]['types']}")
+    #print(f"Game Format: {loaded_cards[0]['gameFormat']}")
+    print(f"Set: {loaded_cards[0]['set']}")
+    print("Legalities:")
+    """
+    So, for this next step, we're dealing with the
+    'legalities' dictionary, which looks like this:
+    {'format': 'Commander', 'legality': 'legal'}
+
+    So, we're going to be using a FOR loop to iterate
+    through the various play formats and print them,
+    along with their corresponding legality status.
+    """
+    for legality in loaded_cards[0]['legalities']:
+        print(f"{legality['format']}: {legality['legality']}")
+
+
+    
 
 
 
--- src/storage/json_storage.py
+++ src/storage/json_storage.py
@@ -1,5 +1,7 @@
 import json
 
+
+# Function for saving cards to cards.json.
 def save_cards(cards, filename="data/cards.json"):
     """
     Saves the list of cards to a JSON file.
@@ -11,3 +13,19 @@ def save_cards(cards, filename="data/cards.json"):
     """
     with open(filename, "w", encoding = "utf-8") as f:
         json.dump(cards, f, indent = 4)
+
+
+# Function for loading cards from cards.json.
+def load_cards(filename="data/cards.json"):
+    """
+    Loads the list of cards from a JSON file.
+
+    Args:
+        filename (str): The name of the file
+        to load the cards from.
+
+    Returns:
+        list: List of card dictionaries.
+    """
+    with open(filename, "r", encoding = "utf-8") as f:
+        return json.load(f)
